5c98d47cf77aadba9aaa5919b41c272da7e6f719
alexandergv <69660314+alexandergv@users.noreply.github.com>
2024-08-01 01:17:25 -0400

    bloqueo de reseñas para un único usuario

--- src/modules/reviews/reviews.controller.ts
+++ src/modules/reviews/reviews.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Param, Get, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Param, Get, Post, Body, UseGuards, Request, Response } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
+import * as jwt from 'jsonwebtoken'
 import { ReviewDto } from './dto/review.dto';
 import { JwtAuthGuard } from '../../guards/jwt-auth.guard';
 import { Review } from './schema/review.schema';
@@ -15,7 +16,32 @@ export class ReviewsController {
   }
 
   @Get('byPlaceId/:placeId')
-  async getReviewsByPlaceId(@Param('placeId') placeId: string): Promise<Review[]> {
-    return this.reviewsService.findAllByPlaceId(placeId);
+  async getReviewsByPlaceId(@Param('placeId') placeId: string, @Request() req, @Response() res): Promise<any> {
+    
+     // Extract token from cookies
+    const token = req.headers.cookie?.split(';')
+      .find(row => row.startsWith('auth_token='))
+      .split('=')[1];
+     let userId = null;
+
+     if (token) {
+       try {
+         // Decode token to get user ID
+         const decoded: any = jwt.verify(token, 'hossio'); // Replace 'hossio' with your secret
+         userId = decoded.sub; // Adjust based on your token payload
+       } catch (error) {
+         console.warn('Invalid token:', error);
+         // Handle invalid token case if necessary
+       }
+     }
+    // Fetch reviews for the place
+    const reviews = await this.reviewsService.findAllByPlaceId(placeId);
+
+    // Check if the user has commented
+    const userReview = reviews.find(review => review.user === userId);
+    return res.json({
+      reviews,
+      userHasCommented: !!userReview
+    });
   }
 }
